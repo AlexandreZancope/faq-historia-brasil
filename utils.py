3b90915c65062d5790fdab46504adf6d4c7d1d55
AlexandreZancope <alexandre.zancope@gmail.com>
2025-06-26 14:09:48 -0300

    26/06/2025

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI  # nova importação
 
 load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 def load_llm():
     return ChatOpenAI(temperature=0.5, model="gpt-3.5-turbo")  # ou "gpt-4"
